e2cff2f5f94cc29424573c06488838b614530015
Hiroharu Hara <hh.25799623@gmail.com>
2020-02-06 00:18:23 +0900

    profileに画像と投稿できる機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,24 @@
 class User < ApplicationRecord
   has_secure_password
+  has_one_attached :profile_picture
+
+  attribute :new_profile_picture
 
   validates :name, presence: true, length: { maximum: 24, minimum: 4 }, uniqueness: true
-  validates :password,
-    presence: true,
-    length: { maximum: 20, minimum: 4 },
-    format: {
-      with: /\A[a-z0-9]+\z/,
-      message: "は小文字英数字で入力して下さい。"
-    }
+
+  validate if: :new_profile_picture do
+    if new_profile_picture.respond_to?(:content_type)
+      unless new_profile_picture.content_type.in?(ALLOWED_CONTENT_TYPES)
+        errors.add(:new_profile_picture, :invalid_image_type)
+      end
+    else
+      errors.add(:new_profile_picture, :invalid)
+    end
+  end
+
+  before_save do
+    if new_profile_picture
+      self.profile_picture = new_profile_picture
+    end
+  end
 end
